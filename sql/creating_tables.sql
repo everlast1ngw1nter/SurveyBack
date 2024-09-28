1f051e692229ff11ecec308049aa53dfda89d25c
everlast1ngw1nter <at07012004@gmail.com>
2024-09-28 17:23:17 +0500

    All tables

--- sql/creating_tables.sql
+++ sql/creating_tables.sql
@@ -1,4 +1,17 @@
 CREATE TABLE users (
     id TEXT,
     email TEXT
-)
+);
+
+CREATE TABLE surveys (
+    id TEXT,
+    user_id TEXT,
+    survey TEXT
+);
+
+CREATE TABLE responses (
+    id TEXT,
+    survey_id TEXT,
+    user_id TEXT,
+    response TEXT
+);
