b1fd142a449cc9dc97ac168c2778452e15cce32d
everlast1ngw1nter <at07012004@gmail.com>
2024-10-10 23:21:24 +0500

    small upd

--- src/main/java/com/example/demo/repos/SurveyRepository.java
+++ src/main/java/com/example/demo/repos/SurveyRepository.java
@@ -1,7 +1,6 @@
 package com.example.demo.repos;
 
 import com.example.demo.models.Survey;
-import com.example.demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
