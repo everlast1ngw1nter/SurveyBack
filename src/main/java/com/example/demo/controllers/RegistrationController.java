2e0417f9804110acbc5b3887387ceddb23cd0c7f
everlast1ngw1nter <at07012004@gmail.com>
2024-10-31 23:00:43 +0500

    pdf base done + missed cors

--- src/main/java/com/example/demo/controllers/RegistrationController.java
+++ src/main/java/com/example/demo/controllers/RegistrationController.java
@@ -4,11 +4,13 @@ import com.example.demo.models.User;
 import com.example.demo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class RegistrationController {
 
     private final UserService userService;
--- src/main/java/com/example/demo/services/PdfService.java
+++ src/main/java/com/example/demo/services/PdfService.java
@@ -2,15 +2,17 @@ package com.example.demo.services;
 
 import com.example.demo.DbService;
 import com.example.demo.dto.AnswersDto;
+import com.example.demo.dto.QuestionsDto;
 import com.example.demo.dto.SurveyDto;
 import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,44 +34,37 @@ public class PdfService implements ReportFileService {
         try {
             PdfWriter.getInstance(document, byteArrayOutputStream);
             document.open();
-            PdfPTable table = new PdfPTable(3);
-            addTableHeader(table);
-            addRows(table);
-            addCustomRows(table);
+            PdfPTable table = new PdfPTable(questions.survey.size());
+            BaseFont baseFont = BaseFont.createFont("fonts\\ofont.ru_Arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+            Font font = new Font(baseFont);
+            addTableHeader(table, questions.survey, font);
+            addRows(table, answers, font);
             document.add(table);
             document.close();
-        } catch (DocumentException e) {
+        } catch (DocumentException | IOException e) {
             e.printStackTrace();
         }
         return byteArrayOutputStream.toByteArray();
     }
 
-    private void addTableHeader(PdfPTable table) {
-        Stream.of("column header 1", "column header 2", "column header 3")
+    private void addTableHeader(PdfPTable table, List<QuestionsDto> questions, Font font) {
+        questions
                 .forEach(columnTitle -> {
                     PdfPCell header = new PdfPCell();
                     header.setBackgroundColor(BaseColor.LIGHT_GRAY);
                     header.setBorderWidth(2);
-//                    header.setPhrase(new Phrase(columnTitle));
+                    header.setPhrase(new Phrase(columnTitle.question, font));
                     table.addCell(header);
                 });
     }
 
-    private void addRows(PdfPTable table) {
-        table.addCell("row 1, col 1");
-        table.addCell("row 1, col 2");
-        table.addCell("row 1, col 3");
-    }
-
-    private void addCustomRows(PdfPTable table) {
-
-        PdfPCell horizontalAlignCell = new PdfPCell(new Phrase("row 2, col 2"));
-        horizontalAlignCell.setHorizontalAlignment(Element.ALIGN_CENTER);
-        table.addCell(horizontalAlignCell);
-
-        PdfPCell verticalAlignCell = new PdfPCell(new Phrase("row 2, col 3"));
-        verticalAlignCell.setVerticalAlignment(Element.ALIGN_BOTTOM);
-        table.addCell(verticalAlignCell);
+    private void addRows(PdfPTable table, List<AnswersDto> answers, Font font) {
+        for(var answer : answers) {
+            for (var answerOnQuestion : answer.answers) {
+                PdfPCell cell = new PdfPCell(new Phrase(answerOnQuestion, font));
+                table.addCell(cell);
+            }
+        }
     }
 
     @Override
