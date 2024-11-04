2a69e2d9d6eed8ea79de5c60bf67df7441bd5ac9
antoshkaxxr <gbanton2004@yandex.ru>
2024-11-04 18:46:29 +0500

    fix datetime

--- src/main/java/com/example/demo/Config.java
+++ src/main/java/com/example/demo/Config.java
@@ -2,8 +2,13 @@ package com.example.demo;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 @Configuration
@@ -12,6 +17,9 @@ public class Config {
     @Bean
     public ObjectMapper objectMapper() {
         var objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(ZonedDateTime.class, new ZonedDateTimeSerializer(DateTimeFormatter.ISO_ZONED_DATE_TIME));
+        objectMapper.registerModule(javaTimeModule);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
